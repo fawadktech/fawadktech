3ea598dbd04e2ca0a3137caa684cf90c97f3b695
Mohammad Fawad <fawadtech10@gmail.com>
2026-08-03 20:41:53 +0500

    initial commit

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const fs = require('fs') ;
 
 const textIn=fs.readFileSync('./starter/txt/input.txt', 'utf-8');
 console.log(textIn);
+
+const textOut =`This is what we know about the avocado: ${textIn}.\n Created on ${Date. now()} `;
+
+fs.writeFileSync('./starter/txt/output.txt',textOut);
+ 
+console.log('File written!');
